278baa7c4277255c1f058fa410632f018120a732
BURGOKA <BURGOKA@ZHA-ITA022-W7.corp.oocl.com>
2017-08-04 11:42:05 +0800

    While loop

--- GitPractice/GitTest/src/com/testingGit/GitPractice.java
+++ GitPractice/GitTest/src/com/testingGit/GitPractice.java
@@ -2,7 +2,13 @@ package com.testingGit;
 
 public class GitPractice {
 	public static void main(String args[]) { 
+		int i = 0;
 		System.out.println("Hello");
 		System.out.println("Kat");
+		
+		
+		while (i < 3) {
+			
+		}
 	}
 }
